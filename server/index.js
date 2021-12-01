fb50ce866b371fff28601376fc6ec37dfdb6dd34
John Duval <john@rockitscienceagency.com>
2021-12-01 14:35:23 -0700

    changed config

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const markHelpful = require('../db/index.js').markHelpful;
 const reportQuestion = require('../db/index.js').reportQuestion;
 const markAnswerHelpful = require('../db/index.js').markAnswerHelpful;
 const reportAnswer = require('../db/index.js').reportAnswer;
-// const db = require('../db/index.js').db;
+const db = require('../db/index.js').db;
 const app = express();
 const PORT = 3000;
 app.use(express.json())
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 
 app.get('/qa/questions', async function (req, res) {
     // console.log('*** question requested ***')
-    const db = await mongoose.connection
+    // const db = await mongoose.connection
         db.collection('questions').aggregate(getQuestionsAndAnswers(parseInt(req.query.question_id)))
         .toArray((err, results) => {
             //console.log(results);
@@ -34,7 +34,7 @@ app.get('/qa/questions', async function (req, res) {
 app.get('/qa/questions/:question_id/answers', async function (req, res) {
     console.log('*** answers requested ***')
 
-    const db = await mongoose.connection
+    // const db = await mongoose.connection
         db.collection('answers').aggregate(getAnswers(parseInt(req.params.question_id)))
         .toArray((err, results) => {
             // console.log(results);

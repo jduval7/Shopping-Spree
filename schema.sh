2fbccf7c26c7f3372d041a13ee1f573d0ab58b36
John Duval <john@rockitscienceagency.com>
2021-12-06 13:57:25 -0700

    new

--- schema.sh
+++ schema.sh
@@ -1,10 +1,10 @@
-mongoimport -d questions-answers -c answers --headerline --columnsHaveTypes --type csv /seed/answers.csv 
-mongoimport -d questions-answers -c questions --headerline --columnsHaveTypes --type csv /seed/questions.csv 
-mongoimport -d questions-answers -c photo --headerline --columnsHaveTypes --type csv /seed/answers_photos.csv 
+mongoimport -d questions-answers -c answers --headerline --columnsHaveTypes --type csv ~/seed/answers.csv 
+mongoimport -d questions-answers -c questions --headerline --columnsHaveTypes --type csv ~/seed/questions.csv 
+mongoimport -d questions-answers -c photo --headerline --columnsHaveTypes --type csv ~/seed/answers_photos.csv 
 
 mongosh questions-answers --eval "db.answers.createIndex({question_id: 1})"
 mongosh questions-answers --eval "db.answers.createIndex({answer_id: 1})"
 mongosh questions-answers --eval "db.questions.createIndex({product_id: 1})"
 mongosh questions-answers --eval "db.questions.createIndex({answer_id: 1})"
 mongosh questions-answers --eval "db.questions.createIndex({question_id: 1})"
-mongosh questions-answers --eval "db.photo.createIndex([{answer_id: 1}])"
+mongosh questions-answers --eval "db.photo.createIndex({answer_id: 1])"

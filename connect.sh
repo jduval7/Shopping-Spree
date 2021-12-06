428253a538247e5c2b5e09e35d752db7e69b565b
John Duval <john@rockitscienceagency.com>
2021-12-06 09:49:04 -0700

    updated public ip4

--- connect.sh
+++ connect.sh
@@ -1,11 +1,11 @@
 case $1 in 
 
 mongo)
-  scp -i SDC.pem ./init.sh ec2-user@ec2-18-191-198-213.us-east-2.compute.amazonaws.com:~/init.sh
-  ssh -i SDC.pem ec2-user@ec2-18-191-198-213.us-east-2.compute.amazonaws.com
+  scp -i SDC.pem ./init.sh ec2-user@ec2-18.216.175.94.us-east-2.compute.amazonaws.com:~/init.sh
+  ssh -i SDC.pem ec2-user@ec2-18.216.175.94.us-east-2.compute.amazonaws.com
   ;;
 node)
-  scp -i SDC.pem ./init.sh ec2-user@ec2-3-144-34-9.us-east-2.compute.amazonaws.com:~/init.sh
-  ssh -i SDC.pem ec2-user@ec2-3-144-34-9.us-east-2.compute.amazonaws.com
+  scp -i SDC.pem ./init.sh ec2-user@ec2-18.220.38.230.us-east-2.compute.amazonaws.com:~/init.sh
+  ssh -i SDC.pem ec2-user@ec2-18.220.38.230.us-east-2.compute.amazonaws.com
   ;;
 esac
--- schema.sh
+++ schema.sh
@@ -2,6 +2,9 @@ mongoimport -d questions-answers -c answers --headerline --columnsHaveTypes --ty
 mongoimport -d questions-answers -c questions --headerline --columnsHaveTypes --type csv /seed/questions.csv 
 mongoimport -d questions-answers -c photo --headerline --columnsHaveTypes --type csv /seed/answers_photos.csv 
 
-mongosh questions-answers --eval "db.answers.createIndexes([{question_id: 1},{answer_id: 1}])"
-mongosh questions-answers --eval "db.questions.createIndexes([{question_id: 1}, {answer_id: 1}])"
-mongosh questions-answers --eval "db.photo.createIndexes([{answer_id: 1}])"
+mongosh questions-answers --eval "db.answers.createIndex({question_id: 1})"
+mongosh questions-answers --eval "db.answers.createIndex({answer_id: 1})"
+mongosh questions-answers --eval "db.questions.createIndex({product_id: 1})"
+mongosh questions-answers --eval "db.questions.createIndex({answer_id: 1})"
+mongosh questions-answers --eval "db.questions.createIndex({question_id: 1})"
+mongosh questions-answers --eval "db.photo.createIndex([{answer_id: 1}])"
